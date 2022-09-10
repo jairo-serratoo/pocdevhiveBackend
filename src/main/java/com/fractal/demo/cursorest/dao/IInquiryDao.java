2fe74fb2cd115f35ae14f2ff4c5a9ff4f5bc9241
Eladio <eladioceledonio.rodriguez.ort@bbva.com>
2022-09-09 23:28:42 -0500

    se inicia configuracion de cero

--- src/main/java/com/fractal/demo/cursorest/dao/IInquiryDao.java
+++
@@ -1,8 +0,0 @@
-//package com.fractal.demo.cursorest.dao;
-//
-//import com.fractal.demo.cursorest.dto.InquiryDTO;
-//import com.fractal.demo.cursorest.model.InquiryModel;
-//
-//public interface IInquiryDao extends IGenericDaoAPI<InquiryModel,InquiryDTO > {
-//
-//}
--- src/main/java/com/fractal/demo/cursorest/dao/ITrackingDao.java
+++
@@ -1,7 +0,0 @@
-//package com.fractal.demo.cursorest.dao;
-//
-//import com.fractal.demo.cursorest.model.TrackingModel;
-//
-//public interface ITrackingDao extends IGenericDaoAPI<TrackingModel> {
-//
-//}
