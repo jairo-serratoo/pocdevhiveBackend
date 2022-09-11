0dacf4e9c80d10dae954d3a2bdedaff07443c3c1
Eladio <eladioceledonio.rodriguez.ort@bbva.com>
2022-09-10 19:46:27 -0500

    recarga de programa

--- src/main/java/com/devhive/demo/DemoApplication.java
+++ src/main/java/com/devhive/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demoJava;
+package com.devhive.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoJavaApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoJavaApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
